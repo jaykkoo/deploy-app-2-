b9731ba4a172d529251e971b5fe376f5dd35a460
jaykkoo <adrien.gutleben@gmail.com>
2024-10-23 00:25:11 +0200

    popo2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,17 +4,16 @@
 sudo rm -rf /home/ubuntu/install
 
 # install CodeDeploy agent
-sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
+sudo apt update
+sudo apt install ruby
+sudo apt install wget
 cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
 
 # update os & install python3
-sudo apt-get update
-sudo apt-get install -y python3 python3-dev python3-pip python3-venv
+sudo apt install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
 # delete app
