0eb33f06891f90bde4b7a2042b0731012b59822f
jaykkoo <adrien.gutleben@gmail.com>
2024-10-23 10:32:24 +0200

    tesd tata

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,4 +1,8 @@
+
+
 sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo apt install -y nginx
 
 sudo cp /home/ubuntu/django-aws_cicd/nginx/nginx.conf /etc/nginx/sites-available/django-aws_cicd
 sudo ln -s /etc/nginx/sites-available/blog /etc/django-aws_cicd/sites-enabled/
